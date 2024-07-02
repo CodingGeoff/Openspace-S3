d5158f1a08ecf6f9c4aaa4a07ae4ffebca50404e
CodingGeoff <120384385+CodingGeoff@users.noreply.github.com>
2024-07-02 16:32:31 +0800

    Add files via upload

--- W1D1(2024.7.2)/POW.py
+++ W1D1(2024.7.2)/POW.py
@@ -1,7 +1,7 @@
 import hashlib
 import time
 
-def compute_hash_with_nonce_1(prefix, difficulty=4):
+def compute_hash_with_nonce(prefix, difficulty):
     nonce = 0
     target = '0' * difficulty  # 目标值，即哈希值以几个0开头
     start_time = time.time()
@@ -23,30 +23,11 @@ def compute_hash_with_nonce_1(prefix, difficulty=4):
 
 # 调用函数，传入'greatgeoff'作为prefix
 print("第一问")
-compute_hash_with_nonce_1('greatgeoff')
+compute_hash_with_nonce('greatgeoff',4)
 
 # ————————————————————————————————————————————————————————————————————————
 
-def compute_hash_with_nonce_2(prefix, difficulty=5):
-    nonce = 0
-    target = '0' * difficulty
-    start_time = time.time()
-
-    while True:
-        # 将prefix和nonce合并后进行哈希运算
-        text = f'{prefix}{nonce}'.encode()
-        sha256_hash = hashlib.sha256(text).hexdigest()
-
-        # 检查哈希值是否满足条件
-        if sha256_hash.startswith(target):
-            time_taken = time.time() - start_time
-            print(f'花费时间: {time_taken:.4f}秒')
-            print(f'满足条件的nonce: {nonce}')
-            print(f'哈希值: {sha256_hash}')
-            break
-
-        nonce += 1
 
 print("第二问")
-compute_hash_with_nonce_2('greatgeoff')
+compute_hash_with_nonce('greatgeoff',5)
 
