bf0ac5daa5589061cacadf97e84574cb734bceda
CodingGeoff <120384385+CodingGeoff@users.noreply.github.com>
2024-07-10 10:56:19 +0800

    Add files via upload

---
+++ W2D2(2024.7.9)/BaseERC20_v1.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+interface IERC1363Receiver {
+    function onTransferReceived(address operator, address from, uint256 value, bytes calldata data) external returns (bytes4);
+}
+
+contract BaseERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 100000000 * (10 ** uint256(decimals));
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "ERC20: transfer to the zero address");
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        if (isContract(_to)) {
+            require(IERC1363Receiver(_to).onTransferReceived(msg.sender, msg.sender, _value, "") == IERC1363Receiver.onTransferReceived.selector, "ERC20: transfer to non ERC1363Receiver implementer");
+        }
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "ERC20: transfer to the zero address");
+        require(balances[_from] >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
+
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        if (isContract(_to)) {
+            require(IERC1363Receiver(_to).onTransferReceived(msg.sender, _from, _value, "") == IERC1363Receiver.onTransferReceived.selector, "ERC20: transfer to non ERC1363Receiver implementer");
+        }
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowances[_owner][_spender];
+    }
+
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+}
---
+++ W2D2(2024.7.9)/BaseERC20_v2.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
+
+contract BaseERC20 {
+    string public tokenName;
+    string public tokenSymbol;
+    uint8 public tokenDecimals;
+    uint256 public tokenTotalSupply;
+
+    mapping(address => uint256) public accountBalances;
+    mapping(address => mapping(address => uint256)) private accountAllowances;
+
+    event TransferEvent(address indexed sender, address indexed recipient, uint256 amount);
+    event ApprovalEvent(address indexed owner, address indexed spender, uint256 amount);
+
+    constructor() {
+        tokenName = "BaseERC20";
+        tokenSymbol = "BERC20";
+        tokenDecimals = 18;
+        tokenTotalSupply = 100000000 * (10 ** uint256(tokenDecimals));
+        accountBalances[msg.sender] = tokenTotalSupply;
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return accountBalances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(accountBalances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+
+        accountBalances[msg.sender] -= amount;
+        accountBalances[recipient] += amount;
+
+        emit TransferEvent(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(accountBalances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(accountAllowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+
+        accountBalances[sender] -= amount;
+        accountBalances[recipient] += amount;
+        accountAllowances[sender][msg.sender] -= amount;
+
+        emit TransferEvent(sender, recipient, amount);
+        return true;
+    }
+
+    function authorizeSpender(address spender, uint256 amount) public returns (bool) {
+        accountAllowances[msg.sender][spender] = amount;
+        emit ApprovalEvent(msg.sender, spender, amount);
+        return true;
+    }
+
+    function getAllowance(address owner, address spender) public view returns (uint256) {
+        return accountAllowances[owner][spender];
+    }
+
+    function transferAndCall(address to, uint256 amount) public returns (bool success) {
+    require(transfer(to, amount));
+    if (to.code.length > 0) { // to is a contract
+        try IERC1363Receiver(to).onTransferReceived(msg.sender, msg.sender, amount, "") returns (bytes4 rtVal) {
+            require(rtVal == IERC1363Receiver.onTransferReceived.selector);
+        } catch {
+            revert();
+        }
+    }
+    return true; // Explicitly return true to indicate success
+}
+
+}
