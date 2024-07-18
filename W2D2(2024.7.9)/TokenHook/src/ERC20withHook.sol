609ed18a4c0e3a24d18a068ba9b884587b79c7f7
CodingGeoff <boc_lc@126.com>
2024-07-18 19:24:55 +0800

    update token hook

--- W2D2(2024.7.9)/TokenHook/src/ERC20withHook.sol
+++ W2D2(2024.7.9)/TokenHook/src/ERC20withHook.sol
@@ -2,25 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC777.sol";
+import "@openzeppelin/contracts/interfaces/IERC777Recipient.sol";
+import "@openzeppelin/contracts/interfaces/IERC777Sender.sol";
+import "@openzeppelin/contracts/interfaces/IERC1820Registry.sol";
 
-interface ITokenReceiver {
-    function tokensReceived(address from, uint256 amount) external;
-}
 
-contract ERC20WithCallback is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract ERC20WithERC777Callbacks is ERC20 {
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
 
-    function transfer (
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal  {
+    constructor(string memory name, string memory symbol) ERC20("GeoToken", "GTH") {}
+
+    function sendCoin(address sender, address recipient, uint256 amount) internal virtual {
         super._transfer(sender, recipient, amount);
-        if (recipient.code.length > 0) {
-            ITokenReceiver(recipient).tokensReceived(sender, amount);
+
+        address implementer = _erc1820.getInterfaceImplementer(recipient, TOKENS_RECIPIENT_INTERFACE_HASH);
+        if (implementer != address(0)) {
+            IERC777Recipient(implementer).tokensReceived(
+                address(this),
+                sender,
+                recipient,
+                amount,
+                "",
+                ""
+            );
         }
     }
-    function mint(address to, uint256 amount) public{
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
--- W2D2(2024.7.9)/TokenHook/src/TokenBank_callback.sol
+++ W2D2(2024.7.9)/TokenHook/src/TokenBank_callback.sol
@@ -2,24 +2,36 @@
 pragma solidity ^0.8.0;
 
 import "./ERC20withHook.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC777.sol";
+import "@openzeppelin/contracts/interfaces/IERC777Recipient.sol";
+import "@openzeppelin/contracts/interfaces/IERC777Sender.sol";
+import "@openzeppelin/contracts/interfaces/IERC1820Registry.sol";
 
-contract TokenBank is ITokenReceiver {
-    ERC20WithCallback public token;
+contract TokenBank is IERC777Recipient {
+    ERC20WithERC777Callbacks TOKEN;
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint256) private _balances;
 
-    constructor(address tokenAddress) {
-        token = ERC20WithCallback(tokenAddress);
+    constructor() {
+        _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
     }
 
-    function tokensReceived(address from, uint256 amount) external override {
-        require(msg.sender == address(token), "Only token contract can call this function");
-        balances[from] += amount;
+    function tokensReceived(
+        address /* operator */,
+        address from,
+        address /* to */,
+        uint256 amount,
+        bytes calldata /* userData */,
+        bytes calldata /* operatorData */
+    ) external override {
+        require(msg.sender == address(TOKEN), "Invalid token");
+        _balances[from] += amount;
     }
 
-    function withdraw(uint256 amount) external {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        token.transfer(msg.sender, amount);
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
     }
 }
